ae0139eac91359677aaf7062bdfca292a4b93943
souren roy <sourenroy.in@gmail.com>
2026-01-31 00:36:58 +0530

    Day 31

---
+++ Day 31/app.js
@@ -0,0 +1,10 @@
+let str = "    help   ";
+
+console.log(str.trim().toUpperCase());
+
+let nam = "apnaCollege";
+nam.slice(4,9) //colle
+nam.indexOf("na"); //2
+nam.replace("apna","our"); //ourCollege
+
+nam.slice(4).replace("l","t").replace("l","t");
