94f65287d2a14bdf79332e6d0b9ac319e3aba601
souren roy <sourenroy.in@gmail.com>
2026-02-11 21:17:07 +0530

    Day 42

---
+++ Day 42/problem.js
@@ -0,0 +1,15 @@
+function calcAVG(a,b,c) {
+    let avg = (a+b+c)/3;
+
+    console.log(avg);
+}
+
+function SUM(a,b) {
+    console.log(a+b);
+}
+
+function printTable(n) {
+    for(let i = n; i<n*11; i+=n) {
+        console.log(i);
+    }
+}
