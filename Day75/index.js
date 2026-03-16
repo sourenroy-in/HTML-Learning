27022bca54ae78a84237bc3e67d7d4ff211ef725
souren roy <sourenroy.in@gmail.com>
2026-03-16 18:44:37 +0530

    Remove node_modules from repository

---
+++ Day75/index.js
@@ -0,0 +1,24 @@
+// Express JS
+
+// 1 Listen for incoming Request 
+// 2 Parse
+// 3 Match Response with routes
+// 4 Response
+
+
+
+// import express from 'express'
+const express = require('express')
+const app = express()
+console.dir(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello Coders')
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+});
+
+
+// *Ports are the logical endpoints of a network connection that is used to exchange information between a web server and a web client.
