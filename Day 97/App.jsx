74ec2ae534811caa112e1a245a9d941c82b6431f
sourenroy-in <sourenroy.in@gmail.com>
2026-04-07 19:09:36 +0530

    msgBox

---
+++ Day 97/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import msgBox from './msgBox';
+
+function App() {
+
+  return (
+    <>
+    <msgBox username = "souren" textColor = "yellow"/>
+    </>
+  )
+}
+
+export default App
---
+++ Day 97/msgBox.jsx
@@ -0,0 +1,6 @@
+export default function msgBox({ username, textColor }) {
+    //let styles = {color: text};
+    return (
+    <h1 style={{color: textColor}}> Hello,{username} </h1 >
+    );
+}
